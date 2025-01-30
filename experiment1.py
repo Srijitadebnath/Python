4709caffe9a034f0e70c3243add9b036cf326370
srijita debnath <SRIJITA.13103@stu.upes.ac.in>
2025-01-30 10:47:20 +0530

    Add files via upload

---
+++ experiment1.py
@@ -0,0 +1,46 @@
+print("hello everyone!")
+print("hello\n" "world")
+
+date="12/05/1999"
+s="'Rohit's date of birth is "
+print(s+date)
+
+x="hello"
+print(x)
+
+x=65.11
+y=90
+print(x,y)
+print(type(x))
+print(type(y))
+
+
+a="Srijita"
+b=" Debnath"
+print(a+b)
+
+a="Srijita"
+b="Debnath"
+c=" (sonu) "
+
+print(a+c+b)
+
+
+name="srijita Debnath"
+sap_id="590013103"
+date_of_birth="12/08/2006"
+address="UPES\n        Bidholi Campus"
+pincode="248007"
+program="Btech CSE"
+Semester=2
+
+print(f"NAME : {name}")
+print(f"SAP ID : {sap_id}")
+print(f"DATE OF BIRTH : {date_of_birth}")
+print(f"ADDRESS : {address}")
+print(f"PROGRAM: {program}")
+print(f"SEMESTER: {Semester}")
+
+
+
+
